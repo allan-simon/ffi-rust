8d08b955870abd204b27da3189ccc08c989484c5
Allan Simon <simona@gobeta.com.cn>
2014-09-28 13:50:06 +0800

    removed use of transmute, void pointer and libc in 'vector of string from rust' example

--- rust_vector_of_string_to_c/rust.rs
+++ rust_vector_of_string_to_c/rust.rs
@@ -1,36 +1,47 @@
 // use to create a .so/.dll
 #![crate_type = "dylib"]
 
-extern crate libc;
-
-use std::mem::transmute;
-use libc::types::common::c95::c_void;
-
-// used to have this use function callable from C
-// 1. no_mangle garanty the name will stay rust_print
-// 2. pub extern garanty the name is exported
+/// create a new vector with some values inside for test
+///
+/// note: we can return directly a Box<Vec<>> and use it
+/// as a pointer from C, because as Vec is not a dynamically sized
+/// type, we're garantueed that Box is a single pointer
+///
+/// note2: after using vector_create from C, we will need to destroy
+/// the value ourselves
 #[no_mangle]
-pub extern fn vector_create() -> *mut c_void {
+pub extern fn vector_create() -> Box<Vec<String>> {
 
     let vector : Box<Vec<String>> = box vec![
         "你好".to_string(),
         "好".to_string()
     ];
 
-    unsafe {transmute(vector)}
+    vector
 }
 
+/// get the size of the vector given in parameter
+///
+///
 #[no_mangle]
 pub extern fn vector_size(vector: &Vec<String>) -> u32 {
     
     vector.len() as u32
 }
 
+/// permit to do vector[index] from C
+///
+/// note: to be 100% correct we should returned
+/// *const libc::c_char, but that would required to use the
+/// crate libc
+///
+/// note2: we need to free the pointer returned by this function
+/// ourselves
 #[no_mangle]
 pub extern fn vector_get(
     vector: &Vec<String> ,
     index: u32
-) -> *const libc::c_char {
+) -> *const i8 {
 
     // (*vector) should not be necessary, there's a bug in current
     // rust (as of end of september 2014) which force us to do that
@@ -38,6 +49,9 @@ pub extern fn vector_get(
 
 }
 
+/// simple debug function to print the content of a vector
+///
+///
 #[no_mangle]
 pub extern fn vector_print(
     vector: &Vec<String>
