e5af6377f3a293e55483a248fd8357dbb6cca4cf
Allan Simon <simona@gobeta.com.cn>
2014-09-28 13:56:10 +0800

    removed use of transmute and C void pointer in rust code of the HashMap example

--- rust_hash_map_to_c_and_back_to_rust_without_runtime/rust.rs
+++ rust_hash_map_to_c_and_back_to_rust_without_runtime/rust.rs
@@ -1,15 +1,15 @@
+// we need this one to be able to change the default hasher
+// in the type declaration of our hashmap
+#![feature(default_type_params)]
 // use to create a .so/.dll
 #![crate_type = "dylib"]
 
-extern crate libc;
 extern crate collections;
-use libc::types::common::c95::c_void;
-use std::mem::transmute;
 use std::collections::HashMap;
 use collections::hash::sip::SipHasher;
 
 #[no_mangle]
-pub extern fn create_hash() -> *mut c_void {
+pub extern fn create_hash() -> Box<HashMap<String, String, SipHasher>> {
     // SipHasher at the opposite of RandomSipHasher does not require
     // rust runtime to work, enabling use to use HashMap without runtime
     // too
@@ -18,12 +18,11 @@ pub extern fn create_hash() -> *mut c_void {
     english_to_french.insert("one".to_string(), "un".to_string());
     english_to_french.insert("two".to_string(), "deux".to_string());
 
-    return unsafe { transmute(english_to_french) };
+    english_to_french
 }
 
 #[no_mangle]
-pub extern fn print_hash(english_to_french_ptr: *mut c_void) {
-    let english_to_french : Box<HashMap<String, String>> = unsafe { transmute(english_to_french_ptr) };
+pub extern fn print_hash(english_to_french: Box<HashMap<String, String, SipHasher>>) {
     for (english, french) in english_to_french.iter() {
         println!(
             "in french '{}' is '{}'",
